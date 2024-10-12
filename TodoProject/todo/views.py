60b3ab6b768292c41733f61c4410d355c2bc7ba6
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-10-12 21:01:01 +0330

    created an api with post method

--- TodoProject/todo/views.py
+++ TodoProject/todo/views.py
@@ -7,8 +7,16 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def all_todos(request: Request):
-    todos = Todo.objects.order_by('priority').all()
-    todo_serializer = TodoSerializer(todos, many=True)
-    return Response(todo_serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        todos = Todo.objects.order_by('priority').all()
+        todo_serializer = TodoSerializer(todos, many=True)
+        return Response(todo_serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(None, status=status.HTTP_400_BAD_REQUEST)
