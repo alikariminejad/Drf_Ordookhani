69dbbaa816512041cbd26e0badbffcf0777e8c89
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-10-15 21:07:38 +0330

    class based view for todos

--- TodoProject/todo/views.py
+++ TodoProject/todo/views.py
@@ -5,6 +5,12 @@ from .models import Todo
 from .serializers import TodoSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+
+
+
+# region Function based views region
+
 
 
 @api_view(['GET', 'POST'])
@@ -42,3 +48,50 @@ def todo_detail_view(request: Request, todo_id):
     elif request.method == 'DELETE':
         todo.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    
+    
+# endregion
+
+# region Class based view
+
+class TodosListApiView(APIView):
+    def get(self, request: Request):
+        todos = Todo.objects.order_by('priority').all()
+        todo_serializer = TodoSerializer(todos, many=True)
+        return Response(todo_serializer.data, status=status.HTTP_200_OK)
+    def post(self, request: Request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(None, status=status.HTTP_400_BAD_REQUEST)
+
+class TodosDetailApiView(APIView):
+    def get_object(self, todo_id:int):
+        try:
+            todo = Todo.objects.get(pk=todo_id)
+            return todo
+        except Todo.DoesNotExist:
+            return Response(None, status.HTTP_404_NOT_FOUND)
+    
+    
+    def get(self, request:Request, todo_id:int):
+        todo = self.get_object(todo_id)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request:Request, todo_id:int):
+        todo = self.get_object(todo_id)
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request:Request, todo_id:int):
+        todo = self.get_object(todo_id)
+        todo.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+# endregion End of region
